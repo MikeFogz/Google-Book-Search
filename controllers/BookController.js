fdc1cd48802e9fd0081d54eca67c7529a2ebcc69
MikeFogz <MikeFogz@gmail.com>
2021-03-11 14:26:25 -0800

    setting up initial routes

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,5 +1,41 @@
+// module.exports = {
+//     test: function(req, res) {
+//         res.send({ msg: "success" });
+//     },
+// };
+
 module.exports = {
-    test: function(req, res) {
-        res.send({ msg: "success" });
+    findAll: function(req, res) {
+      db.Book
+        .find(req.query)
+        .sort({ date: -1 })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     },
-};
+    findById: function(req, res) {
+      db.Book
+        .findById(req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+      db.Book
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+      db.Book
+        .findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+      db.Book
+        .findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    }
+  };
+  
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
-const { test } = require("../controllers/BookController")
+const { test } = require("../controllers/bookController")
 
 router.get("/api/test", test);
 
+// return all saved books
+router.get("/api/books", );
+
+//save new book to db
+router.post("/api/books", test);
+
+//delete book from db
+router.delete("/api/books/:id", test);
+
+//save new book to db
+router.get("*", test);
 
 module.exports = router;
